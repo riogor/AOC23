b4bda17b2727216f0b6eb9cfc51015da80d05135
riogor <astronaft100@yandex.ru>
2023-12-07 10:28:35 +0300

    day 7

---
+++ 7/1.cpp
@@ -0,0 +1,94 @@
+#include "templ.hpp"
+
+map<char, int> cards = {{'A', 14}, {'K', 13}, {'Q', 12}, {'J', 11}, {'T', 10}, {'9', 9}, {'8', 8},
+                        {'7', 7},  {'6', 6},  {'5', 5},  {'4', 4},  {'3', 3},  {'2', 2}};
+
+const int HAND_SIZE = 5;
+
+bool isCardLess(const char lhs, const char rhs)
+{
+	return cards[lhs] < cards[rhs];
+}
+
+int getHandType(const string &hand)
+{
+	map<char, int> mp;
+	for (char c : hand)
+		if (mp.contains(c))
+			mp[c]++;
+		else
+			mp[c] = 1;
+
+	int cnt[6] = {};
+
+	for (auto &[key, val] : mp)
+		cnt[val]++;
+
+	if (cnt[5])
+		return 7;
+
+	if (cnt[4])
+		return 6;
+
+	if (cnt[3] && cnt[2])
+		return 5;
+
+	if (cnt[3])
+		return 4;
+
+	if (cnt[2] == 2)
+		return 3;
+
+	if (cnt[2])
+		return 2;
+
+	return 1;
+}
+
+struct game
+{
+	string hand;
+	ll bid;
+	int type;
+
+	game() = default;
+	game(const string &hand, const ll bid) : hand(hand), bid(bid), type(getHandType(hand))
+	{
+	}
+
+	bool operator<(const game &oth) const
+	{
+		if (type != oth.type)
+			return type < oth.type;
+
+		for (int i = 0; i < HAND_SIZE; i++)
+		{
+			if (hand[i] == oth.hand[i])
+				continue;
+
+			return isCardLess(hand[i], oth.hand[i]);
+		}
+
+		return false;
+	}
+};
+
+void solve()
+{
+	vector<game> games;
+
+	{
+		string s;
+
+		while (getline(cin, s))
+			games.pb(game(s.substr(0, 5), stoll(s.substr(6))));
+	}
+
+	sort(games.begin(), games.end());
+
+	ll sum = 0;
+	for (ll i = 0; i < games.size(); i++)
+		sum += (i + 1) * games[i].bid;
+
+	cout << sum;
+}
